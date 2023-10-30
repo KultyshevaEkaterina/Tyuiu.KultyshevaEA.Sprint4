9389a684b924c0afe8fc8ff91d9f76171fe6a9ad
Kultysheva Ekaterina <kultysheva.ekaterina@gmail.com>
2023-10-30 18:24:04 +0500

    Add project files.

---
+++ Tyuiu.KultyshevaEA.Sprint4.Task0.V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KultyshevaEA.Sprint4.Task0.V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KultyshevaEA.Sprint4.Task0.V18.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8379a8f8-7ea6-463b-97bd-37999b94a514")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KultyshevaEA.Sprint4.Task0.V18/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KultyshevaEA.Sprint4.Task0.V18.Lib;
+
+namespace Tyuiu.KultyshevaEA.Sprint4.Task0.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнила: Култышева Е. А. | ИИПБ-23-3";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнила: Култышева Екатерина Александровна | ИИПБ-23-3                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать сумму          *");
+            Console.WriteLine("* нечетных элементов массива.  {9, 8, 7, 6, 5, 7, 3, 2, 7, 3}             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] numsArray = { 9, 8, 7, 6, 5, 7, 3, 2, 7, 3 };
+
+            Console.WriteLine("Исходный массив: ");
+            for (int i = 0; i<= numsArray.Length -1;i++)
+            {
+                Console.WriteLine(numsArray[i]);
+            }
+
+
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма нечетных элементов массива = " + ds.GetSumOddArrEl(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
