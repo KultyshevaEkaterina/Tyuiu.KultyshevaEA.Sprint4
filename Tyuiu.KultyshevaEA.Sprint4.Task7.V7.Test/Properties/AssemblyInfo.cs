8f4709bf0d4d1242e91e33f732485b6a77ab33a0
Kultysheva Ekaterina <kultysheva.ekaterina@gmail.com>
2023-11-11 10:34:00 +0500

    Task7

---
+++ Tyuiu.KultyshevaEA.Sprint4.Task7.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KultyshevaEA.Sprint4.Task7.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KultyshevaEA.Sprint4.Task7.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1f87fe59-7522-4f0e-b9ce-2d000e419c36")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KultyshevaEA.Sprint4.Task7.V7/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KultyshevaEA.Sprint4.Task7.V7.Lib;
+
+namespace Tyuiu.KultyshevaEA.Sprint4.Task7.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            int m = 2;
+
+            string str = "31415926";
+
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнила: Култышева Е. А. | ИИПБ-23-3";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнила: Култышева Екатерина Александровна | ИИПБ-23-3                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр '31415926'.                           *");
+            Console.WriteLine("* Преобразуйте ее в матрицу 4 на 2 и подсчитайте количество               *");
+            Console.WriteLine("* нечетных чисел в матрице.                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int index = 0;
+
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i<n;i++)
+            {
+                for( int j = 0; j<m;j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(n, m, str);
+
+            Console.WriteLine("Количество нечетных элементов: " + res);
+            Console.ReadKey();
+
+        }
+    }
+}
